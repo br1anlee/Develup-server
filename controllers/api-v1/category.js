35e94505a55fc106987220e11730df9d58c56e54
“jamelscott” <itsjamelscott@gmail.com>
2022-03-27 00:46:36 -0700

    createad route to allow user to update a deck and its card

--- controllers/api-v1/category.js
+++ controllers/api-v1/category.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const db = require('../../models');
-const requiresToken = require('../requiresToken');
-const user = require('../../models/user');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const db = require("../../models");
+const requiresToken = require("../requiresToken");
+const user = require("../../models/user");
 
 // GET - View all categories
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const allCategories = await db.Category.find({});
 
   res.json(allCategories);
 });
 
 // POST - create a new deck and cards (may include category)
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     // Check if a category exists
     const categoryCheck = await db.Category.findOne({
@@ -43,7 +43,7 @@ router.post('/', async (req, res) => {
       await categoryCheck.save();
       res.json({ categoryCheck });
 
-    // if a category doesn't exist, create a new category, new deck and new cards
+      // if a category doesn't exist, create a new category, new deck and new cards
     } else {
       const newCategory = await db.Category.create({
         name: req.body.name,
@@ -70,78 +70,69 @@ router.post('/', async (req, res) => {
       await newCategory.save();
       res.json({ newCategory });
     }
-  // Log an error to the server's console if there is an issue creating anything
+    // Log an error to the server's console if there is an issue creating anything
   } catch (err) {
     console.log(err);
-    res.status(503).json({ msg: 'server error 503 🔥😭' });
+    res.status(503).json({ msg: "server error 503 🔥😭" });
   }
 });
 
 // DELETE - Delete a category
-router.delete('/:id', async (req, res) => {
-  try{
+router.delete("/:id", async (req, res) => {
+  try {
     const deletedCategory = await db.Category.findByIdAndDelete({
       _id: req.params.id,
     });
     const allCategories = await db.Category.find({});
     res.json(allCategories);
-  }catch(err){
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
 });
 
 //DELETE - Delete a deck
-router.delete('/:categoryId/deck/:deckId', async (req, res)=>{
-
-  const deckId = req.params.deckId
-  const categoryId = req.params.categoryId
-
-  const category = await db.Category.findById({
-    _id:categoryId
-  })
-
-try{
-
- category.decks.id(deckId).remove()
-  await category.save()
-
-
-res.json({category})
- 
-}catch(err){
-  console.log(err)
-}
-
-
-})
-
-//PUT - update a deck
-router.put('/:categoryId/deck/:deckId', async (req, res)=>{
-  const deckId = req.params.deckId
-  const categoryId = req.params.categoryId
+router.delete("/:categoryId/deck/:deckId", async (req, res) => {
+  const deckId = req.params.deckId;
+  const categoryId = req.params.categoryId;
 
   const category = await db.Category.findById({
-    _id:categoryId
-  })
+    _id: categoryId,
+  });
+
+  try {
+    category.decks.id(deckId).remove();
+    await category.save();
+
+    res.json({ category });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//PUT - update a deck and cards
+router.put("/:categoryId/deck/:deckId", async (req, res) => {
+  const deckId = req.params.deckId;
+  const categoryId = req.params.categoryId;
+
+  const category = await db.Category.findById({
+    _id: categoryId,
+  });
 
   let deckIdx = category.decks.findIndex((object) => {
     return object.id === deckId;
   });
 
-  try{
+  try {
+    category.decks[deckIdx].deckName = req.body.deckName;
+    category.decks[deckIdx].cards = req.body.cards;
 
-    category.decks[deckIdx].deckName = req.body.deckName
-
-    await category.save()
-
-    res.json({category})
+    await category.save();
 
 
-
-  }catch(err){
-    console.log(err)
+    res.json({ category });
+  } catch (err) {
+    console.log(err);
   }
-
-})
+});
 
 module.exports = router;
